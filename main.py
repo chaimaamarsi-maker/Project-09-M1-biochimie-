22677e94be3571937c845b454a3da0a2cdeb900e
chaimaamarsi-maker <chaimaamarsi@gmail.com>
2025-12-10 16:30:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Project-09-M1-biochimie-
+# for Biology Master tlemcen ...13/12/2025
+#Marsi Chaimae, master 1 biochimie  13/12/2025
+#Les membres de groupe 
+#Guermat fatima zohra 
+#Maasri nihel
+#Tikhemarine sihem
+#Benhamida manel
+import pandas as pd
